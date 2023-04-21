a8c2c410c3e92725263f2934d5370e186476ea2f
Francisco Maria Calisto <francisco.calisto@tecnico.ulisboa.pt>
2023-04-21 01:42:56 +0100

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -1,5 +1,12 @@
-import csv
-with open('dataset/overview.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+import pydicom
+import matplotlib.pyplot as plt
+
+# Load the DICOM image
+ds = pydicom.dcmread('dataset/00378d36-1798bf0e-f78e72b8-726cf0da-f229e157/IMAGES/IM0')
+
+# Get the pixel data as a NumPy array
+pixel_data = ds.pixel_array
+
+# Show the image using matplotlib
+plt.imshow(pixel_data, cmap=plt.cm.bone)
+plt.show()
